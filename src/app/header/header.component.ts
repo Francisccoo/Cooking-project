0b60ce3c4758b43db3c4572fb871317787827cbf
FCR <franchesco2009@gmail.com>
2019-10-15 19:08:14 +0200

    S3 - E51 - Añadiendo contenido al Componente Recetas

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+	recipes: Recipe[] = [
+
+		new Recipe('A Test Recipe', 'This is simply a test', 
+			'https://www.gimmesomeoven.com/wp-content/uploads/2018/02/A-Couple-Cooks-Cauliflower-Black-Bean-Tacos-Recipe-with-Chipotle-Crema-5.jpg'),
+
+	];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  collapsed = true;
 
 }

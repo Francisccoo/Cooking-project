638651341980e40ef25ca3141a537798ebeba4ae
FCR <franchesco2009@gmail.com>
2019-10-29 17:27:29 +0100

    Navegación HEADER "supuestamente" funcional, revisar y seguir aprendiendo.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
-
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featuredSelect = new EventEmitter<string>(); //The '()' means that is instantiated
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(feature: string) {
+  	this.featuredSelect.emit(feature);
   }
 
 }

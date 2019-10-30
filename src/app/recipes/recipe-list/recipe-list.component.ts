54505e93ff4e2060484f2a40247ff40e081c8388
FCR30 <franchesco2009@gmail.com>
2019-10-30 12:32:58 +0100

    Añadidos enlaces a recetas y receta al detalle, pronto el añadiremos la inserción de recetas nuevas.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -9,16 +9,24 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
 
 		new Recipe('A Test Recipe', 'This is simply a test', 
 			'https://m6udbe444b-flywheel.netdna-ssl.com/wp-content/uploads/2018/09/YLR-logo@3x.png'),
 
+    new Recipe('Another Test Recipe', 'This is simply a test', 
+      'https://m6udbe444b-flywheel.netdna-ssl.com/wp-content/uploads/2018/09/YLR-logo@3x.png'),
 	];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
   collapsed = true;
